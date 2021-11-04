782cd3ebac8764002d8075b4596016b6fc21c922
jmirfield <justin.mirfield@gmail.com>
2021-11-04 13:54:50 -0400

    Added GET methods to retrieve tasks/users by one

--- src/index.js
+++ src/index.js
@@ -21,7 +21,17 @@ app.get('/users', (req, res) => {
     User.find({}).then((users) => {
         res.send(users)
     }).catch((err) => {
-        res.status(400).send(err)
+        res.status(500).send(err)
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    User.findById(_id).then((user) => {
+        if(!user)return res.status(404).send()
+        res.send(user)
+    }).catch((err) => {
+        res.status(500).send(err)
     })
 })
 
@@ -31,6 +41,24 @@ app.post('/tasks', (req, res) => {
         res.status(201).send(task)
     }).catch((error) => {
         res.status(400).send(error)
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((err) => {
+        res.status(500).send(err)
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+    Task.findById(_id).then((task) => {
+        if(!task)return res.status(404).send()
+        res.send(task)
+    }).catch((err) => {
+        res.status(500).send(err)
     })
 })
 
