9d992e2608c3e20ee944a272eb42409490c4e721
jmirfield <justin.mirfield@gmail.com>
2021-11-08 09:56:13 -0500

    HTTP methods -> changed to async

--- src/index.js
+++ src/index.js
@@ -9,75 +9,53 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
-    user.save().then(() => {
+    try{
+        await user.save()
         res.status(201).send(user)
-    }).catch((error) => {
+    } catch (e) {
         res.status(400).send(error)
-    })
+    }
 })
 
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
+app.get('/users', async (req, res) => {
+    try{
+        const users = await User.find({})
         res.send(users)
-    }).catch((err) => {
-        res.status(500).send(err)
-    })
+    } catch (e) {
+        res.status(500).send(e)
+    }
 })
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id
-    User.findById(_id).then((user) => {
+    try {
+        const user = await User.findById(_id)
         if(!user)return res.status(404).send()
         res.send(user)
-    }).catch((err) => {
-        res.status(500).send(err)
-    })
+    } catch (e) {
+        res.status(500).send(e)
+    }
 })
 
-app.post('/tasks', (req, res) => {
-    const task = new Task(req.body)
-    task.save().then(() => {
-        res.status(201).send(task)
-    }).catch((error) => {
-        res.status(400).send(error)
-    })
-})
-
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => {
-        res.send(tasks)
-    }).catch((err) => {
-        res.status(500).send(err)
-    })
-})
-
-app.get('/tasks/:id', (req, res) => {
-    const _id = req.params.id
-    Task.findById(_id).then((task) => {
-        if(!task)return res.status(404).send()
-        res.send(task)
-    }).catch((err) => {
-        res.status(500).send(err)
-    })
-})
-
-app.post('/recipes', (req, res) => {
+app.post('/recipes', async (req, res) => {
     const recipe = new Recipe(req.body)
-    recipe.save().then(() => {
+    try {
+        await recipe.save()
         res.status(201).send(recipe)
-    }).catch((error) => {
-        res.status(400).send(error)
-    })
+    } catch (e) {
+        res.status(400).send(e)
+    }
 })
 
-app.get('/recipes', (req, res) => {
-    Recipe.find({}).then((recipes) => {
+app.get('/recipes', async (req, res) => {
+    try {
+        const recipes = await Recipe.find({})
         res.send(recipes)
-    }).catch((err) => {
-        res.status(500).send(err)
-    })
+    } catch (e) {
+        res.status(500).send(e)
+    }
 })
 
 app.listen(PORT, () => {
